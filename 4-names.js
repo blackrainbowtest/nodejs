582d39b024e8251d1e40bb937d48cd512a97a94b
blackrainbowtest <araqelyan.aram.96@gmail.com>
2024-12-05 18:32:20 +0400

    add

---
+++ 4-names.js
@@ -0,0 +1,6 @@
+// local
+const secret = "SUPER SECRET"
+const john = 'john'
+const peter = 'peter'
+
+console.log(module);
---
+++ app.js
@@ -0,0 +1,6 @@
+// CommonJS, - every file is module (by deafult)
+// Modules		- Encapsulated Code (only share minimum)
+
+sayHi("susan")
+sayHi(john)
+sayHi(peter)
