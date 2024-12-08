2f6978f5bd833eb75d3dd5cc71b7147af708fb15
blackrainbowtest <araqelyan.aram.96@gmail.com>
2024-12-08 19:31:31 +0400

    fix login request frontend

--- frontend/src/features/auth/user_login/LoginAPI.js
+++ frontend/src/features/auth/user_login/LoginAPI.js
@@ -1,37 +1,23 @@
 // src/auth/user_login/LoginAPI.js
-import { createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
-import apiUrl from 'app/base/base_url';
-import { addError, addNotification, setLoading } from 'features/global/GlobalSlice';
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import apiUrl from "app/base/base_url";
+import {
+  addError,
+  addNotification,
+  setLoading,
+} from "features/global/GlobalSlice";
 
 /**
  * Login user
  */
 export const loginUser = createAsyncThunk(
-  'user/loginUser',
+  "user/loginUser",
   async (userData, { dispatch, rejectWithValue }) => {
     try {
       dispatch(setLoading(true));
-      const response = await axios.get(`${apiUrl}/users/login`, {
-        params: {
-          email: userData.email,
-        },
-      });
-      if (response.data.length) {
-        // get first user
-        const user = response.data[0];
-
-        // check if user password is correct
-        if (user.password === userData.password) {
-          // patch user and add token
-          const updatedUserResponse = await axios.patch(`${apiUrl}/users/${user.id}`, {
-            token: userData.token,
-          });
-          return updatedUserResponse.data;
-        }
-      }
-      // If no user found or password is incorrect
-      throw new Error('Invalid email or password.');
+      const response = await axios.get(`${apiUrl}/users/login`, userData);
+      return response.data;
     } catch (err) {
       dispatch(addError(err.message));
       return rejectWithValue(err.message);
@@ -44,18 +30,18 @@ export const loginUser = createAsyncThunk(
  * Verify token need add JWT token later
  */
 export const verifyToken = createAsyncThunk(
-  'user/verifyToken',
+  "user/verifyToken",
   async (token, { dispatch, rejectWithValue }) => {
     try {
       dispatch(setLoading(true));
       const response = await axios.get(`${apiUrl}/users/token`, {
-        params: { token }
+        params: { token },
       });
       if (response.data.length > 0) {
-        dispatch(addNotification("Login success."))
+        dispatch(addNotification("Login success."));
         return response.data;
       } else {
-        throw new Error('Invalid token.');
+        throw new Error("Invalid token.");
       }
     } catch (err) {
       dispatch(addError(err.message));
